ab8f32ffc273c5a3d77f4cd5f1eb5502e58d637c
drobotun <drobotun@xakep.ru>
2020-04-20 17:03:31 +0300

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,3 +60,18 @@ def setup(app):
     app.add_stylesheet('theme_overrides.css')
 
 master_doc = 'index'
+
+latex_elements = {
+# The paper size ('letterpaper' or 'a4paper').
+#'papersize': 'letterpaper',
+
+# The font size ('10pt', '11pt' or '12pt').
+#'pointsize': '10pt',
+
+# Additional stuff for the LaTeX preamble.
+'preamble': '\\usepackage[utf8]{inputenc}',
+'babel': '\\usepackage[russian]{babel}',
+'cmappkg': '\\usepackage{cmap}',
+'fontenc': '\usepackage[T1,T2A]{fontenc}',
+'utf8extra':'\\DeclareUnicodeCharacter{00A0}{\\nobreakspace}',
+}
